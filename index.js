4eaa69cc6a6aaaa6d8b8a9a6acbf5b96e02938f6
Simrri Salazar <ssimrri@gmail.com>
2021-11-24 15:21:22 -0600

    commit tree c236c5fda83b7103b22061e8b6acdbe2da2b93dd

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 1
+let num2 = 62
+let num3 = 44
+let num4 = 10
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() * 10)
+
+const mod = num3 % num4
+
+const max = Math.max(1, 2, 3, 4, 20)
